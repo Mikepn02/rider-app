4d140171850c430067678907a502e9bfc6612cd7
mikepn02 <nzaberamikepeter@gmail.com>
2024-08-21 15:58:24 +0200

    initial commit

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { Stack } from 'expo-router'
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default Layout
