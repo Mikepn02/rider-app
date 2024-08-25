260ad39e4d22f5afb7e225352a83b0512003cd7f
mikepn02 <nzaberamikepeter@gmail.com>
2024-08-25 14:32:00 +0200

    updated

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -2,8 +2,10 @@ import { View, Text, Alert, ActivityIndicator } from "react-native";
 import React, { useState } from "react";
 import CustomButton from "./CustomButton";
 import { useStripe } from "@stripe/stripe-react-native";
+import { fetchAPI } from "@/lib/fetch";
+import { PaymentProps } from "@/types/type";
 
-const Payment = () => {
+const Payment = ({fullName , email ,amount , driverId , rideTime} : PaymentProps) => {
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
@@ -11,8 +13,37 @@ const Payment = () => {
   const initializePaymentSheet = async () => {
     try {
       const confirmHandler = async (paymentMethod: any, shouldSavePaymentMethod: any, intentCreationCallback: any) => {
-        // Add logic for handling payment confirmation
+        const { paymentIntent , customer} = await fetchAPI("/(api)/(create)/create", {
+          method: "POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            name: fullName || email.split("@")[0].slice(0,7),
+            email: email,
+            amount: amount,
+            paymentMethodId: paymentMethod.id
+          })
+        })
+
+        if(paymentIntent.client_secret){
+          const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              payment_method_id: paymentMethod.id,
+              payment_intent_id: paymentIntent.id,
+              customer_id: customer
+            })
+          })
+          if(result.client_secret){
+            
+          }
+        }
       };
+
 
       const { error } = await initPaymentSheet({
         merchantDisplayName: "Example, Inc.",
